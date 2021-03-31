f133fda395cebebb76cc6e7bc7687bde8af871cd
Riki2216 <81208877+Riki2216@users.noreply.github.com>
2021-03-31 13:39:22 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,11 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
     drink = ->(left, drinked){
-            return drinked if left.zero?
-            i = left / 3
-            d = i.zero? ? left : i * 3
-            drink.(left - d + i, drinked + d)
-        }
-        drink.(amount, 0)
+        return drinked 
+        if left.zero?
+          i = left / 3
+          d = i.zero? ? left : i * 3
+          drink.(left - d + i, drinked + d)
+      }
+    drink.(amount, 0)
   end
 end
